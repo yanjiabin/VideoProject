0cbbedaad2ad63399601221108930a3740b13670
yanjiabin <yanjiabin159357@163.com>
2021-02-04 23:03:08 +0800

    第一次提交项目

---
+++ app/src/main/java/com/asa/kotlin/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.asa.kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    override  fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+}
